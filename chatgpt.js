17bd95b524f346f07493eb1506b9b12e7b290df8
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2023-06-21 09:31:23 +0800

    Add files via upload

--- chatgpt.js
+++ chatgpt.js
@@ -32,111 +32,67 @@
     //properties
     onCustomWidgetBeforeUpdate(oChangedProperties) {
       console.log("onCustomWidgetBeforeUpdate");
-      //debugger;
     }
 
-      //Auto call setter & getter function from Bulider/Styling Panel
     onCustomWidgetAfterUpdate(oChangedProperties) {
       console.log("onCustomWidgetAfterUpdate");
       console.log(oChangedProperties);
-      //debugger;
-      if("modelId" in oChangedProperties){
-        this.modelId = oChangedProperties["modelId"];
-      }
-      if("dimensionList" in oChangedProperties){
-        this.dimensionList = oChangedProperties["dimensionList"];
-      }
-      if("measureList" in oChangedProperties){
-        this.measureList = oChangedProperties["measureList"];
-      }
     }
 
     //methods
-    getPrompt() {
-      return this.prompt;
-    }
-    setPrompt(newPrompt) {
-      this.prompt = newPrompt;
-    }
     _renderAnalyzeButton() {
-        let buttonSlot = document.createElement("div");
-        buttonSlot.slot = "analyze_button";
-        this.appendChild(buttonSlot);
+      let buttonSlot = document.createElement("div");
+      buttonSlot.slot = "analyze_button";
+      this.appendChild(buttonSlot);
 
-        this._analyzeButton = new sap.m.Button({
-            text: "分析数据",
-            press: () => {
-                this.analyze();
-            }
-        });
-        this._analyzeButton.placeAt(buttonSlot);
+      this._analyzeButton = new sap.m.Button({
+        text: "分析数据",
+        press: () => {
+          this.analyze();
+        },
+      });
+      this._analyzeButton.placeAt(buttonSlot);
     }
-    prepareData() {
-      let datasource = this.getDataSource();
-      let data = datasource.data;
-      let metadata = datasource.metadata;
+    _prepareData() {
+      if (this.source === "DataBinding") {
+        let datasource = this.getDataSource();
+        let data = datasource.data;
+        let metadata = datasource.metadata;
 
-      if (!data || !data.length) {
-        Application.showMessage(Type.Error, "当前数据源未读取到任何数据");
-        return;
-      }
-
-      let dimensions = metadata.dimensions;
-      let measures = metadata.mainStructureMembers;
-      let feeds = metadata.feeds;
-      let feedDimensions = feeds.dimensions.values;
-      let feedMeasures = feeds.measures.values;
-      let dataset = [];
-
-      for (let i = 0; i < data.length; i++) {
-        let row = {};
-        for (let j = 0; j < feedDimensions.length; j++) {
-          row[dimensions[feedDimensions[j]].description] =
-            data(i)[feedDimensions[j]].label;
+        if (!data || !data.length) {
+          Application.showMessage(Type.Error, "当前数据源未读取到任何数据");
+          return;
         }
-        for (let j = 0; j < feedMeasures.length; j++) {
-          row[measures[feedMeasures[j]].label] =
-            data(i)[feedMeasures[j]].raw.toString() +
-            " " +
-            data(i)[feedMeasures[j]].unit;
+
+        let dimensions = metadata.dimensions;
+        let measures = metadata.mainStructureMembers;
+        let feeds = metadata.feeds;
+        let feedDimensions = feeds.dimensions.values;
+        let feedMeasures = feeds.measures.values;
+        let dataset = [];
+
+        for (let i = 0; i < data.length; i++) {
+          let row = {};
+          for (let j = 0; j < feedDimensions.length; j++) {
+            row[dimensions[feedDimensions[j]].description] =
+              data(i)[feedDimensions[j]].label;
+          }
+          for (let j = 0; j < feedMeasures.length; j++) {
+            row[measures[feedMeasures[j]].label] =
+              data(i)[feedMeasures[j]].raw.toString() +
+              " " +
+              data(i)[feedMeasures[j]].unit;
+          }
+          dataset.push(row);
         }
-        dataset.push(row);
+        this.jsonData = "{data: " + JSON.stringify(dataset) + "}";
       }
-      this.dataset = dataset;
-    }
-    preparePrompt() {
-      if (this.data) {
-      }
-      let datastr = "{";
-      let data = this.dataset;
-      for (let i = 0; i < data.lengrh; i++) {
-        let rowstr = JSON.stringify(data(i));
-        datastr = datastr + rowstr;
-      }
-      datastr = datastr + "}";
-      let prompt = this.prompt;
-      prompt = prompt + "。数据集如下：" + datastr;
-      this.prompt = prompt;
+      this.prompt = this.prompt + this.jsonData;
+      console.log(this.prompt);
     }
     analyze() {
-      if(this.getDimensions().length === 0){
-        this.dimensionList = this.dimensionList;
-      }
-      if(this.getMeasures().length === 0){
-        this.measureList = this.measureList;
-      }
-      console.log("analyze");
-      console.log(this.dataBindings);
-      console.log(this.dataBindings.getDataBinding("customDataBinding"));
-      console.log(this.customDataBinding);
-      console.log(this.getDataSource());
-      console.log(this.getDimensions());
-      console.log(this.getMeasures());
-      //debugger;
-      this.prepareData();
-      this.preparePrompt();
+      this._prepareData();
 
-      
       let reqbody = {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: this.prompt }],
@@ -173,54 +129,91 @@
       });  */
     }
 
-    //Datasource and DataBinding
+    getPrompt() {
+      return this.prompt;
+    }
+    setPrompt(newPrompt) {
+      this.prompt = newPrompt;
+    }
+    getJson() {
+      return this.jsonData;
+    }
+    setJson(newVal) {
+      this.jsonData = newVal;
+    }
     getDataSource() {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").getDataSource();
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .getDataSource();
       }
-      
+    }
+    getModel() {
+      return this.modelId;
     }
     setModel(modelId) {
-      if(this.dataBindings){
-        this.dataBindings.getDataBinding("customDataBinding").setModel(modelId);
+      if (this.dataBindings) {
+        let result = this.dataBindings
+          .getDataBinding("customDataBinding")
+          .setModel(modelId);
+        if (result) {
+          console.log("setModel: " + modelId + ",result: Success");
+          this.modelId = modelId;
+        } else {
+          console.log("setModel: " + modelId + ",result: Failed");
+        }
       }
     }
     openSelectModelDialog() {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").openSelectModelDialog();
-      }   
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .openSelectModelDialog();
+      }
     }
     getDimensions() {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").getDimensions("dimensions");
-      }   
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .getDimensions("dimensions");
+      }
     }
     addDimension(dimensionId) {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").addDimensionToFeed("dimensions", dimensionId);
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .addDimensionToFeed("dimensions", dimensionId);
       }
     }
     removeDimension(dimensionId) {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").removeDimension(dimensionId);
-      }    
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .removeDimension(dimensionId);
+      }
     }
     getMeasures() {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").getMembers("measures");
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .getMembers("measures");
       }
     }
     addMeasure(measureId) {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").addMemberToFeed("measures", measureId);
-      }  
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .addMemberToFeed("measures", measureId);
+      }
     }
     removeMeasure(measureId) {
-      if(this.dataBindings){
-        return this.dataBindings.getDataBinding("customDataBinding").removeMember(measureId);
-      }    
+      if (this.dataBindings) {
+        return this.dataBindings
+          .getDataBinding("customDataBinding")
+          .removeMember(measureId);
+      }
     }
-    dbg(){
+    dbg() {
       console.log(this.customDataBinding);
     }
 
@@ -243,46 +236,23 @@
     set dataset(newData) {
       this._props.dataset = newData;
     }
-    get source(){
+    get source() {
       return this._props.source;
     }
-    set source(newSource){
+    set source(newSource) {
       this._props.source = newSource;
     }
-    get modelId(){
-      console.log("get modelId()");
+    get modelId() {
       return this._props.modelId;
     }
-    set modelId(newModel){
-      console.log("set modelId()");
+    set modelId(newModel) {
       this._props.modelId = newModel;
-      this.setModel(newModel)
     }
-    get dimensionList(){
-      return this._props.dimensionList;
+    get jsonData() {
+      return this._props.jsonData;
     }
-    set dimensionList(newDimStr){
-      console.log("set dimensionList");
-      this._props.dimensionList = newDimStr;
-      let dimArr = newDimStr.split(",");
-      for(let i = 0; i < dimArr.length; i++){
-        let dim = dimArr[i];
-        console.log(dim);
-        this.addDimension(dim);
-      }
-    }
-    get measureList(){
-      return this._props.measureList;
-    }
-    set measureList(newMeaStr){
-      console.log("set measureList");
-      this._props.measureList = newMeaStr;
-      let meaArr = newMeaStr.split(",");
-      for(let i = 0; i < meaArr.length; i++){
-        let mea = meaArr[i];
-        console.log(mea);
-        this.addMeasure(mea);
-      }
+    set jsonData(newVal) {
+      this._props.jsonData = newVal;
     }
   }
 
